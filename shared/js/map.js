ccac703c9e1c504cff814a812f50f82a6c73b204
masplanning <cboulanger@mas.org>
2026-08-20 17:59:32 +0200

    Positron base map

--- shared/js/map.js
+++ shared/js/map.js
@@ -19,7 +19,7 @@ const map = new maplibregl.Map({
     container: "map",
     style: "https://tiles.openfreemap.org/styles/positron",
     center: config.center || [-73.99, 40.75],
-    zoom: config.zoom || 12
+    zoom: config.zoom || 14
 });
 
 
